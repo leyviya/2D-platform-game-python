97711417835dc74a451c83fa958dfde1332c28b9
Leyla Abdullayeva <leyla_abdullayeva_2002@mail.ru>
2022-04-13 15:56:25 +0300

    Add files via upload
    Level 1 map designs & alternatives.
    All maps have been created in the "Tiled" maps editor and all maps for each level should have separate ".tsx" & ".tmx" files.

---
+++ Level 1/level1new.tsx
@@ -0,0 +1,70 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.0" name="level1new" tilewidth="128" tileheight="128" tilecount="22" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="128" height="128" source="boxCrate_double (2).png"/>
+ </tile>
+ <tile id="1">
+  <image width="128" height="128" source="grassCenter (1).png"/>
+ </tile>
+ <tile id="2">
+  <image width="128" height="128" source="signRight.png"/>
+ </tile>
+ <tile id="3">
+  <image width="128" height="128" source="doorClosed_top.png"/>
+ </tile>
+ <tile id="4">
+  <image width="128" height="128" source="doorClosed_mid.png"/>
+ </tile>
+ <tile id="5">
+  <image width="128" height="128" source="bush.png"/>
+ </tile>
+ <tile id="6">
+  <image width="128" height="128" source="cactus.png"/>
+ </tile>
+ <tile id="7">
+  <image width="128" height="128" source="bridgeA.png"/>
+ </tile>
+ <tile id="8">
+  <image width="128" height="128" source="lavaTop_high.png"/>
+ </tile>
+ <tile id="9">
+  <image width="128" height="128" source="lava.png"/>
+ </tile>
+ <tile id="10">
+  <image width="128" height="128" source="grass_sprout.png"/>
+ </tile>
+ <tile id="11">
+  <image width="128" height="128" source="grassHill_right.png"/>
+ </tile>
+ <tile id="12">
+  <image width="128" height="128" source="grassCliffAlt_left.png"/>
+ </tile>
+ <tile id="13">
+  <image width="128" height="128" source="grassMid.png"/>
+ </tile>
+ <tile id="14">
+  <image width="128" height="128" source="grassCorner_right (1).png"/>
+ </tile>
+ <tile id="15">
+  <image width="128" height="128" source="coinGold.png"/>
+ </tile>
+ <tile id="16">
+  <image width="128" height="128" source="grassCorner_right.png"/>
+ </tile>
+ <tile id="17">
+  <image width="128" height="128" source="grassCliff_right.png"/>
+ </tile>
+ <tile id="18">
+  <image width="128" height="128" source="grassLeft (1).png"/>
+ </tile>
+ <tile id="19">
+  <image width="128" height="128" source="grass.png"/>
+ </tile>
+ <tile id="20">
+  <image width="128" height="128" source="grassRight (1).png"/>
+ </tile>
+ <tile id="21">
+  <image width="128" height="128" source="grassCliffAlt_right (1).png"/>
+ </tile>
+</tileset>
---
+++ Level 1/map2_level_1.tsx
@@ -0,0 +1,82 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.0" name="level4" tilewidth="128" tileheight="128" tilecount="26" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="128" height="128" source="../PycharmProjects/2DD/platform_tutorial/images/tiles/brickGrey.png"/>
+ </tile>
+ <tile id="1">
+  <image width="128" height="128" source="../PycharmProjects/2DD/platform_tutorial/images/tiles/planetHalf_left.png"/>
+ </tile>
+ <tile id="2">
+  <image width="128" height="128" source="images/waterTop_low (1).png"/>
+ </tile>
+ <tile id="3">
+  <image width="128" height="128" source="../PycharmProjects/2DD/platform_tutorial/images/tiles/water.png"/>
+ </tile>
+ <tile id="4">
+  <image width="128" height="128" source="../PycharmProjects/2DD/platform_tutorial/images/tiles/planetCenter.png"/>
+ </tile>
+ <tile id="5">
+  <image width="128" height="128" source="../PycharmProjects/2DD/platform_tutorial/images/tiles/rock.png"/>
+ </tile>
+ <tile id="6">
+  <image width="128" height="128" source="../PycharmProjects/2DD/platform_tutorial/images/tiles/planetMid.png"/>
+ </tile>
+ <tile id="7">
+  <image width="128" height="128" source="../PycharmProjects/2DD/platform_tutorial/images/tiles/planetLeft.png"/>
+ </tile>
+ <tile id="8">
+  <image width="128" height="128" source="../PycharmProjects/2DD/platform_tutorial/images/tiles/planetRight.png"/>
+ </tile>
+ <tile id="9">
+  <image width="128" height="128" source="../PycharmProjects/2DD/platform_tutorial/images/tiles/planetCorner_right.png"/>
+ </tile>
+ <tile id="10">
+  <image width="128" height="128" source="../PycharmProjects/2DD/platform_tutorial/images/tiles/spikes.png"/>
+ </tile>
+ <tile id="11">
+  <image width="128" height="128" source="../PycharmProjects/2DD/platform_tutorial/images/tiles/planetHalf_right.png"/>
+ </tile>
+ <tile id="12">
+  <image width="128" height="128" source="../PycharmProjects/2DD/platform_tutorial/images/tiles/plantPurple.png"/>
+ </tile>
+ <tile id="13">
+  <image width="128" height="128" source="../PycharmProjects/2DD/platform_tutorial/images/tiles/planetHalf_mid.png"/>
+ </tile>
+ <tile id="14">
+  <image width="128" height="128" source="../PycharmProjects/2DD/platform_tutorial/images/tiles/doorClosed_mid.png"/>
+ </tile>
+ <tile id="15">
+  <image width="128" height="128" source="../PycharmProjects/2DD/platform_tutorial/images/tiles/doorClosed_top.png"/>
+ </tile>
+ <tile id="16">
+  <image width="128" height="128" source="../PycharmProjects/2DD/platform_tutorial/images/tiles/planetHill_right.png"/>
+ </tile>
+ <tile id="17">
+  <image width="128" height="128" source="../PycharmProjects/2DD/platform_tutorial/images/tiles/planetHill_left.png"/>
+ </tile>
+ <tile id="19">
+  <image width="128" height="128" source="../PycharmProjects/2DD/platform_tutorial/images/tiles/planetCliff_left.png"/>
+ </tile>
+ <tile id="20">
+  <image width="128" height="128" source="../PycharmProjects/2DD/platform_tutorial/images/tiles/planetCliff_right.png"/>
+ </tile>
+ <tile id="21">
+  <image width="128" height="128" source="../PycharmProjects/2D/venv/Lib/site-packages/arcade/resources/images/items/coinSilver.png"/>
+ </tile>
+ <tile id="22">
+  <image width="128" height="128" source="../PycharmProjects/2DD/platform_tutorial/images/tiles/planetCorner_left.png"/>
+ </tile>
+ <tile id="23">
+  <image width="128" height="128" source="../PycharmProjects/2DD/platform_tutorial/images/tiles/planetCliffAlt_left.png"/>
+ </tile>
+ <tile id="24">
+  <image width="128" height="128" source="../PycharmProjects/2DD/platform_tutorial/images/tiles/planetCliffAlt_right.png"/>
+ </tile>
+ <tile id="25">
+  <image width="128" height="128" source="../PycharmProjects/2DD/platform_tutorial/images/tiles/ladderMid.png"/>
+ </tile>
+ <tile id="26">
+  <image width="128" height="128" source="../PycharmProjects/2DD/platform_tutorial/images/tiles/ladderTop.png"/>
+ </tile>
+</tileset>
