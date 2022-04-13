506e563f613e57c41548f53c6ad099b532ce27f7
Leyla Abdullayeva <leyla_abdullayeva_2002@mail.ru>
2022-04-13 15:59:28 +0300

    Add files via upload
    Level 4,5,6 map designs & alternatives.
    All maps have been created in the "Tiled" maps editor and all maps for each level should have separate ".tsx" & ".tmx" files.

---
+++ Other levels/level5.tsx
@@ -0,0 +1,46 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.0" name="level5" tilewidth="128" tileheight="128" tilecount="14" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="128" height="128" source="../Downloads/planet.png"/>
+ </tile>
+ <tile id="1">
+  <image width="64" height="64" source="../Downloads/coinSilver_test.png"/>
+ </tile>
+ <tile id="2">
+  <image width="128" height="128" source="../Downloads/planetCliff_left.png"/>
+ </tile>
+ <tile id="3">
+  <image width="128" height="128" source="../Downloads/planetCliff_right.png"/>
+ </tile>
+ <tile id="4">
+  <image width="128" height="128" source="../Downloads/planetLeft.png"/>
+ </tile>
+ <tile id="5">
+  <image width="128" height="128" source="../Downloads/waterTop_low.png"/>
+ </tile>
+ <tile id="6">
+  <image width="128" height="128" source="../Downloads/rock (1).png"/>
+ </tile>
+ <tile id="7">
+  <image width="128" height="128" source="../Downloads/plantPurple.png"/>
+ </tile>
+ <tile id="8">
+  <image width="128" height="128" source="../Downloads/spikes.png"/>
+ </tile>
+ <tile id="9">
+  <image width="128" height="128" source="../Downloads/coinSilver.png"/>
+ </tile>
+ <tile id="10">
+  <image width="128" height="128" source="../Downloads/planetCenter.png"/>
+ </tile>
+ <tile id="11">
+  <image width="128" height="128" source="../Downloads/planetHill_right (1).png"/>
+ </tile>
+ <tile id="12">
+  <image width="128" height="128" source="../Downloads/planetCorner_right.png"/>
+ </tile>
+ <tile id="13">
+  <image width="128" height="128" source="../Downloads/planetMid.png"/>
+ </tile>
+</tileset>
