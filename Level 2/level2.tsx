218416f2413db7d3d9d67defe2b0508d8021a56c
Leyla Abdullayeva <leyla_abdullayeva_2002@mail.ru>
2022-04-13 15:58:31 +0300

    Add files via upload
    "Level 2" map design in 2 different ways.
    All maps have been created in the "Tiled" maps editor and all maps for each level should have separate ".tsx" & ".tmx" files.

---
+++ Level 2/level2.tsx
@@ -0,0 +1,79 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.0" name="level1new" tilewidth="128" tileheight="128" tilecount="25" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="128" height="128" source="../../PycharmProjects/2D/venv/Lib/site-packages/arcade/resources/images/tiles/boxCrate_double.png"/>
+ </tile>
+ <tile id="1">
+  <image width="128" height="128" source="../../PycharmProjects/2D/venv/Lib/site-packages/arcade/resources/images/tiles/grassCenter.png"/>
+ </tile>
+ <tile id="2">
+  <image width="128" height="128" source="../../PycharmProjects/2D/venv/Lib/site-packages/arcade/resources/images/tiles/signRight.png"/>
+ </tile>
+ <tile id="3">
+  <image width="128" height="128" source="../../PycharmProjects/2D/venv/Lib/site-packages/arcade/resources/images/tiles/doorClosed_top.png"/>
+ </tile>
+ <tile id="4">
+  <image width="128" height="128" source="../../PycharmProjects/2D/venv/Lib/site-packages/arcade/resources/images/tiles/doorClosed_mid.png"/>
+ </tile>
+ <tile id="5">
+  <image width="128" height="128" source="../../PycharmProjects/2D/venv/Lib/site-packages/arcade/resources/images/tiles/bush.png"/>
+ </tile>
+ <tile id="6">
+  <image width="128" height="128" source="../../PycharmProjects/2D/venv/Lib/site-packages/arcade/resources/images/tiles/cactus.png"/>
+ </tile>
+ <tile id="7">
+  <image width="128" height="128" source="../../PycharmProjects/2D/venv/Lib/site-packages/arcade/resources/images/tiles/bridgeA.png"/>
+ </tile>
+ <tile id="8">
+  <image width="128" height="128" source="../../PycharmProjects/2D/venv/Lib/site-packages/arcade/resources/images/tiles/lavaTop_high.png"/>
+ </tile>
+ <tile id="9">
+  <image width="128" height="128" source="../../PycharmProjects/2D/venv/Lib/site-packages/arcade/resources/images/tiles/lava.png"/>
+ </tile>
+ <tile id="10">
+  <image width="128" height="128" source="../../PycharmProjects/2D/venv/Lib/site-packages/arcade/resources/images/tiles/grass_sprout.png"/>
+ </tile>
+ <tile id="11">
+  <image width="128" height="128" source="../../PycharmProjects/2D/venv/Lib/site-packages/arcade/resources/images/tiles/grassHill_right.png"/>
+ </tile>
+ <tile id="12">
+  <image width="128" height="128" source="../../PycharmProjects/2D/venv/Lib/site-packages/arcade/resources/images/tiles/grassCliffAlt_left.png"/>
+ </tile>
+ <tile id="13">
+  <image width="128" height="128" source="../../PycharmProjects/2D/venv/Lib/site-packages/arcade/resources/images/tiles/grassMid.png"/>
+ </tile>
+ <tile id="14">
+  <image width="128" height="128" source="../../PycharmProjects/2D/venv/Lib/site-packages/arcade/resources/images/tiles/grassCorner_right.png"/>
+ </tile>
+ <tile id="15">
+  <image width="128" height="128" source="../../PycharmProjects/2D/venv/Lib/site-packages/arcade/resources/images/items/coinGold.png"/>
+ </tile>
+ <tile id="16">
+  <image width="128" height="128" source="../../PycharmProjects/2D/venv/Lib/site-packages/arcade/resources/images/tiles/grassCorner_right.png"/>
+ </tile>
+ <tile id="17">
+  <image width="128" height="128" source="../../PycharmProjects/2D/venv/Lib/site-packages/arcade/resources/images/tiles/grassCliff_right.png"/>
+ </tile>
+ <tile id="18">
+  <image width="128" height="128" source="../../PycharmProjects/2D/venv/Lib/site-packages/arcade/resources/images/tiles/grassLeft.png"/>
+ </tile>
+ <tile id="19">
+  <image width="128" height="128" source="../../PycharmProjects/2D/venv/Lib/site-packages/arcade/resources/images/tiles/grass.png"/>
+ </tile>
+ <tile id="20">
+  <image width="128" height="128" source="../../PycharmProjects/2D/venv/Lib/site-packages/arcade/resources/images/tiles/grassRight.png"/>
+ </tile>
+ <tile id="21">
+  <image width="128" height="128" source="../../PycharmProjects/2D/venv/Lib/site-packages/arcade/resources/images/tiles/grassCliffAlt_right.png"/>
+ </tile>
+ <tile id="22">
+  <image width="128" height="128" source="../../PycharmProjects/2D/venv/Lib/site-packages/arcade/resources/images/tiles/ladderTop.png"/>
+ </tile>
+ <tile id="23">
+  <image width="128" height="128" source="../../PycharmProjects/2D/venv/Lib/site-packages/arcade/resources/images/tiles/bridgeB.png"/>
+ </tile>
+ <tile id="24">
+  <image width="128" height="128" source="../../PycharmProjects/2D/venv/Lib/site-packages/arcade/resources/images/tiles/ladderMid.png"/>
+ </tile>
+</tileset>
